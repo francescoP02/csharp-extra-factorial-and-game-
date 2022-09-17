0e9d3813a80d08a1b26a49054a847d9123a613b4
francescoP02 <file.partipilo@gmail.com>
2022-09-17 15:56:30 +0200

    Added factorial calculator

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Scrivi un numero");
+
+long numero = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Il fattoriale è {Fattoriale(numero)}");
+
+//Funzione ricorsiva
+
+long Fattoriale(long number)
+{
+    if (number <= 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return (number * Fattoriale(number - 1));
+    }
+
+}
